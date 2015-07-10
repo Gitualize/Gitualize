7f64a7c953c134b67f8a3c8a5df4084470c76076
Xue Hu <dxuehu@gmail.com>
2015-07-10 11:12:50 -0700

    Feat: fix backend stuff and add some sample fields, server saves an entry upon start

--- server/server.js
+++ server/server.js
@@ -3,14 +3,26 @@ var app = express();
 var db = require('./db/config.js');
 // var Repos = require('./db/collections/repos.js');
 // var Commits = require('./db/collections/commits.js');
-// var Repo = require('./db/models/repo.js');
-// var Commit = require('./db/models/commit.js');
+ var Repo = require('./db/models/repo.js');
+ var Commit = require('./db/models/commit.js');
+//var util = require('./utils');
 
-app.get('/', function(){
+//app.get('/', function(){
 
+//});
+
+app.use(express.static(__dirname + '/../client'));
+
+var server = app.listen(process.env.PORT || 3000, function(){
 });
 
-var server = app.listen(3000, function(){
-  console.log('connected');
+new Commit({sha: '123', user: 'dani'}).save().then(function(commit) {
+  console.log('saved commit: ', commit);
 });
+
+
+
+
+
+
 
