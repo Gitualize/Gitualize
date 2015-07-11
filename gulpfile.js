4becca788223f4cf1d738f0a9d49aec9b52f593c
Devon <devonharvey@gmail.com>
2015-07-11 12:22:47 -0700

    (feat)Set up gulp file to work with browserify and JSX

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 var gulp = require('gulp');
-
 var watch = require('gulp-watch');
-
-var bower = require('bower');
-
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
-
 var jshint = require('gulp-jshint');
 var jasmine = require('gulp-jasmine');
-
 var notify = require('gulp-notify');
-
 var shell = require('gulp-shell');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify'); 
+var source = require('vinyl-source-stream');
+
+
+
 
 var paths = {
   backend: ['./server/**/*.js'],
@@ -41,15 +41,30 @@ gulp.task('test', function(){
     .pipe(jasmine());
 });
 
+gulp.task('browserify', function() {
+    var bundler = browserify({
+        entries: ['./client/js/app.js'],
+        transform: [reactify], // We want to convert JSX to normal javascript
+        debug: true, // Gives us sourcemapping
+        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+    });
+    var watcher  = watchify(bundler);
+
+    return watcher
+    .on('update', function () { // When any files update
+        var updateStart = Date.now();
+        console.log('Updating!');
+        watcher.bundle() // Create new bundle that uses the cache for high performance
+        .pipe(source('./client/js/app.js'))
+    // This is where you add uglifying etc.
+        .pipe(gulp.dest('./client/build'));
+        console.log('Updated!', (Date.now() - updateStart) + 'ms');
+    })
+    .bundle() // Create the initial bundle when starting the task
+    .pipe(source('./client/js/app.js'))
+    .pipe(gulp.dest('./client/build'));
+});
+
 gulp.task('build', ['jshint', 'test']);
 
-gulp.task('default', ['build']);
-
-// gulp.task('watch', function(){
-//   gulp.src(['./client/**/*.js']);
-//   watch('./server/**/*.js', function(){
-
-//   });
-// });
-
-//gulp.task('',[]);
+gulp.task('default',['browserify']);
