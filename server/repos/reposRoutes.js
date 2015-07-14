05452cd1d76335ebf23e9b39cb8213cfd9148b3c
Xue Hu <dxuehu@gmail.com>
2015-07-14 15:36:46 -0700

    (Refactor) refactor repo code and start commits

--- server/repos/reposRoutes.js
+++ server/repos/reposRoutes.js
@@ -3,5 +3,6 @@ var reposController = require('./reposController.js');
 //return all users
 
 module.exports = function(app) {
-  // app.get('/:repo', reposController.getRepo); //get a repo with possible filters
+   //app.get('/:repo', reposController.getRepo); //get a repo with possible filters
+   //what is this??
 };
