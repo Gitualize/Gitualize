ac7c2eae83eda5b60f4f0a4b5a2bc9b0b80bd5d1
Xue Hu <dxuehu@gmail.com>
2015-07-14 15:36:46 -0700

    (Refactor) refactor repo code and start commits

--- server/repos/reposUtils.js
+++ server/repos/reposUtils.js
@@ -1,71 +1,75 @@
+var Promise = require('bluebird');
 var db = require('../db/config');
 var Repos = require('../db/collections/repos');
 var Repo = require('../db/models/repo');
 var utils = require('../users/usersUtils');
 
+var retrieveRepos = function(name, callback) {
+  new Repo({
+    full_name : name
+  }).fetch().then(function(found) {
+    if (found) {
+      callback(null, found.attributes);
+    } else {
+      console.log('could not find repo: ', name);
+      callback(null, null);
+    }
+  })
+  .catch(function(error) {
+    console.log('error:', error);
+  });
+};
+
+var storeRepo = function(repo, callback) {
+  var name = repo.name;
+  var full_name = repo.full_name;
+  var owner = repo.owner;
+
+  new Repo({
+    full_name: full_name
+  }).fetch().then(function(found) {
+    if (found) {
+      callback(null, found.attributes);
+      console.log('repo already found:', name);
+    } else {
+      var newRepo = new Repo({
+        full_name: full_name,
+        name: name,
+        owner: owner
+      });
+      newRepo.save().then(function(newRepo) {
+        Repos.add(newRepo);
+        callback(null, newRepo.attributes);
+      })
+      .catch(function(error) {
+        console.log('error:', error);
+      });
+    }
+  })
+  .catch(function(error) {
+    console.log('error:', error);
+  });
+},
+
 module.exports = {
   //get a repo from DB by reponame with possible filters
-  retrieveRepos: function(name, callback) {
-    Repo
-      .where({owner: name})
-      .fetchAll().then(function(repos) {
-        if (found) {
-          callback(null, repos);
-        } else {
-          console.log('found no repos for: ', name);
-          callback(null, null);
-        }
-      })
-      .catch(function(error) {
-        console.log('error:', error);
-      });
-  },
+  //retrieveRepos: function(name, callback) {
+  //Repo
+  //.where({owner: name})
+  //.fetchAll().then(function(repos) {
+  //if (found) {
+  //callback(null, repos);
+  //} else {
+  //console.log('found no repos for: ', name);
+  //callback(null, null);
+  //}
+  //})
+  //.catch(function(error) {
+  //console.log('error:', error);
+  //});
+  //},
 
-  retrieveRepo: function(name, callback) {
-    new Repo({
-        full_name : name
-      }).fetch().then(function(found) {
-        if (found) {
-          callback(null, found.attributes);
-        } else {
-          console.log('could not find repo: ', name);
-          callback(null, null);
-        }
-      })
-      .catch(function(error) {
-        console.log('error:', error);
-      });
-  },
-
+  retrieveRepo: Promise.promisify(retrieveRepo),
+  storeRepo: Promise.promisify(storeRepo)
   //store a new repo in DB https://developer.github.com/v3/repos/#get
-  storeRepo: function(repo, callback) {
-    var name = repo.name;
-    var full_name = repo.full_name;
-    var owner = repo.owner;
-
-    new Repo({
-        full_name: full_name
-      }).fetch().then(function(found) {
-        if (found) {
-          callback(null, found.attributes);
-          console.log('repo already found:', name);
-        } else {
-          var newRepo = new Repo({
-            full_name: full_name,
-            name: name,
-            owner: owner
-          });
-          newRepo.save().then(function(newRepo) {
-            Repos.add(newRepo);
-            callback(null, newRepo.attributes);
-          })
-          .catch(function(error) {
-            console.log('error:', error);
-          });
-        }
-      })
-      .catch(function(error) {
-        console.log('error:', error);
-      });
-  },
 };
--- server/users/usersRoutes.js
+++ server/users/usersRoutes.js
@@ -1,10 +1,9 @@
 var usersController = require('./usersController.js');
-var reposController = require('../repos/reposController.js');
 
 //return all users
 
 module.exports = function(app) {
-  app.get('/:user/repos/:repo', reposController.getRepo); //get a user's repo with possible filters
-  app.get('/:user/repos', reposController.getRepos) //get a list of user's repos
-  app.get('/:user', usersController.getUser); //get a user
+  //app.get('/:user/repos/:repo', reposController.getRepo); //get a user's repo with possible filters
+  //app.get('/:user/repos', reposController.getRepos) //get a list of user's repos
+  //app.get('/:user', usersController.getUser); //get a user
 };
