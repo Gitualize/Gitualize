c5a43a66c185f74e3ec5f7460f53f20ac807a74f
Devon <devonharvey@gmail.com>
2015-07-17 12:32:09 -0700

    (feat)Path renders buttons which call updateCurrentPath

--- client/js/views/path.react.jsx
+++ client/js/views/path.react.jsx
@@ -1,8 +1,17 @@
 var React = require('react');
 
 var Path = React.createClass({
+  handleClick: function () {
+    this.props.updateCurrentPath(['d', 'e', 'f']);
+  },
+
   render: function () {
-    return <div>Path: {this.props.currentPath}
+    var fullPath = this.props.currentPath.map(function(folder) {
+      return <button onClick={this.handleClick}>
+        {folder}
+      </button>
+    }.bind(this));
+    return <div>Path: {fullPath}
     </div>
   }
 });
