a97454fe3c60f7af9e9a3e64e790564fa2ef2651
Alberto d'Souza <ardsouza1@gmail.com>
2015-07-18 11:45:03 -0700

    (fix) fixed path styling and funcitonality

--- client/js/views/path.react.jsx
+++ client/js/views/path.react.jsx
@@ -1,4 +1,6 @@
+var ReactBootstrap = require('react-bootstrap');
 var React = require('react');
+var Button = ReactBootstrap.Button;
 
 var Path = React.createClass({
   handleClick: function (index) {
@@ -8,14 +10,21 @@ var Path = React.createClass({
   render: function () {
     var context = this;
     var fullPath = this.props.currentPath.map(function(folder, index) {
-      return <button onClick={function () {context.handleClick(index)}}>
-        {folder}
-      </button>
+      return (
+          <span>
+            <Button bsSize="xsmall" bsStyle="link" onClick={function () {context.handleClick(index)}}>
+              {folder}
+            </Button>
+            <Button bsSize="xsmall" bsStyle="link">/</Button>
+          </span>
+        )
     }.bind(this));
-    return <div>Path: 
-      <button onClick={function () {context.handleClick(-1)}}> / </button>
-      {fullPath}
-    </div>
+    return (
+        <div>Path: 
+          <Button bsSize="xsmall" bsStyle="link" onClick={function () {context.handleClick(-1)}}>/</Button>
+          {fullPath}
+        </div>
+      )
   }
 });
 
