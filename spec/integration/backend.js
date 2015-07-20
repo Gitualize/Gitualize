5c3003badf9af66a4efdfa9685ffd2c8c5ec298d
Terry <tchan247@gmail.com>
2015-07-20 12:29:13 -0700

    (bug) remove users controller test

--- spec/integration/backend.js
+++ spec/integration/backend.js
@@ -62,28 +62,6 @@ describe('Repos Controller', function(){
 
 });
 
-describe('Users Controller', function(){
-
-  it('should add users to the database', function(done){
-
-    request('http://127.0.0.1:3000/getAccessToken?repoFullName=tchan247/blog-project', function(error, response, body){
-      new User({}).fetch().then(function(item){
-        // console.log('ITEMS!!!!');
-        // console.log(item);
-
-        expect(item).toBeDefined();
-        if(item) {
-          item.destroy();
-        }
-        done();
-      });
-    });
-
-  });
-
-});
-
-
 describe('Authentication Controller', function(){
 
   it('should redirect to github when trying to get a repo first time', function(done){
