6ecc3531eda23013da7dcc3db571628b162c3ddd
Devon <devonharvey@gmail.com>
2015-07-21 12:14:04 -0700

    (feat)Create CommitInfo component

---
+++ client/js/views/commit_info.react.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var CommitInfo = React.createClass({
+  render: function () {
+    return (
+      <div>
+
+      </div>
+      )
+  }
+});
+
+module.exports = CommitInfo;
