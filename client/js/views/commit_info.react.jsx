206aeb73112c15427837e27f34ba767a6f075b03
Devon <devonharvey@gmail.com>
2015-07-21 17:19:53 -0700

    (feat)CommitInfo displays avatar of commiter for current commit

--- client/js/views/commit_info.react.jsx
+++ client/js/views/commit_info.react.jsx
@@ -3,9 +3,10 @@ var React = require('react');
 var CommitInfo = React.createClass({
   render: function () {
     var message = this.props.currentCommit.message;
+    var imageURL = this.props.currentCommit.avatarUrl + '&s=48';
     return (
       <div>
-        Commit Info:
+        <img style={{'height': '48px', 'width': '48px'}} src={imageURL}/>
         {message}
       </div>
       )
