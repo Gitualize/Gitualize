d651d96c7ecbc7c3418ce88629f51306a792bd17
Alberto d'Souza <ardsouza1@gmail.com>
2015-07-23 14:55:15 -0700

    (feat) added octocat

--- client/js/views/navbar.react.jsx
+++ client/js/views/navbar.react.jsx
@@ -2,24 +2,29 @@ var ReactBootstrap = require('react-bootstrap');
 var React = require('react');
 var Navbar = ReactBootstrap.Navbar;
 var Nav = ReactBootstrap.Nav;
-// var NavItem = ReactBootstrap.NavItem;
+var NavItem = ReactBootstrap.NavItem;
 // var DropdownButton = ReactBootstrap.DropdownButton;
 // var MenuItem = ReactBootstrap.MenuItem;
 
 var NavHead = React.createClass({
+  styles: {
+    imgStyle : {'height': 16+'px', 'width': 16+'px'}
+  },
+
   render: function() {
     return (
-    <Navbar brand={<a href="#">Gitualize</a>}>
-      <Nav>
-
-      </Nav>
-    </Navbar>)
+        <Navbar brand={<a href="#">Gitualize</a>}>
+          <Nav right>
+            <NavItem eventKey={1} href='https://github.com/'><img style={this.styles.imgStyle} src='../../pics/Github-Mark-32px.png'/></NavItem>
+          </Nav>
+        </Navbar>
+      )
   }
 });
 
 module.exports = NavHead;
 
-        // <NavItem eventKey={1} href='#'>Link</NavItem>
+        
         // <NavItem eventKey={2} href='#'>Link</NavItem>
         // <DropdownButton eventKey={3} title='Dropdown'>
         //   <MenuItem eventKey='1'>Action</MenuItem>
