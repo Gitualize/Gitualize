47a7c7822a541201c00b7b608293e13742bcf735
Alberto d'Souza <ardsouza1@gmail.com>
2015-07-23 15:38:25 -0700

    (fix) reduced navbar bottom margin

--- client/js/views/navbar.react.jsx
+++ client/js/views/navbar.react.jsx
@@ -3,19 +3,18 @@ var React = require('react');
 var Navbar = ReactBootstrap.Navbar;
 var Nav = ReactBootstrap.Nav;
 var NavItem = ReactBootstrap.NavItem;
-// var DropdownButton = ReactBootstrap.DropdownButton;
-// var MenuItem = ReactBootstrap.MenuItem;
 
 var NavHead = React.createClass({
   styles: {
-    imgStyle : {'height': 16+'px', 'width': 16+'px'}
+    imgStyle : {'height': 16+'px', 'width': 16+'px'},
+    navStyle : {marginBottom: '0'}
   },
 
   render: function() {
     return (
-        <Navbar brand={<a href="#">Gitualize</a>}>
+        <Navbar style={this.styles.navStyle} brand={<a href="#">Gitualize</a>}>
           <Nav right>
-            <NavItem eventKey={1} href='https://github.com/'><img style={this.styles.imgStyle} src='../../pics/Github-Mark-32px.png'/></NavItem>
+            <NavItem href='https://github.com/'><img style={this.styles.imgStyle} src='../../pics/Github-Mark-64px.png'/></NavItem>
           </Nav>
         </Navbar>
       )
@@ -23,13 +22,3 @@ var NavHead = React.createClass({
 });
 
 module.exports = NavHead;
-
-        
-        // <NavItem eventKey={2} href='#'>Link</NavItem>
-        // <DropdownButton eventKey={3} title='Dropdown'>
-        //   <MenuItem eventKey='1'>Action</MenuItem>
-        //   <MenuItem eventKey='2'>Another action</MenuItem>
-        //   <MenuItem eventKey='3'>Something else here</MenuItem>
-        //   <MenuItem divider />
-        //   <MenuItem eventKey='4'>Separated link</MenuItem>
-        // </DropdownButton>
