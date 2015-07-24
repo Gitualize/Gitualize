69ca7fda56dd835b28f7e05ecc674d9620808902
Xue Hu <dxuehu@gmail.com>
2015-07-23 19:24:28 -0700

    start refactoring dir view

--- client/js/views/directory.react.jsx
+++ client/js/views/directory.react.jsx
@@ -5,6 +5,10 @@ var Glyphicon = ReactBootstrap.Glyphicon;
 var Well = ReactBootstrap.Well;
 
 var Directory = React.createClass({
+  handleClick: function(path) {
+    debugger;
+    this.props.updateCurrentPath(path);
+  },
   map: function(obj, callback) {
     var array = [];
     for (var key in obj) {
@@ -14,33 +18,34 @@ var Directory = React.createClass({
     }
     return array;
   },
-
+  buildInner: function(value, depth, path) {
+   return (<div>{this.buildDirectory(value, depth, path)}</div>);
+  },
   buildDirectory: function(obj, depth, path) {
     var style = {
       paddingLeft: (depth*10) + 'px'
-    }
+    };
     return this.map(obj,function(value, key) { //TODO refactor
-      if (value.deleted) {
-        return;
-      }
-      if (value.isFolder) {
-        return (
-          <div>
-            <div style={style}><Glyphicon glyph="folder-open" /><Button bsSize="xsmall" onClick={ function() {this.props.updateCurrentPath(path + key)}.bind(this) } bsStyle="link">{key}</Button></div>
-            <div>{this.buildDirectory(value, depth+1,path + key + '/') }</div>
-          </div>
-        )
-      } else {
-        return <div style={style}><Glyphicon glyph="file" /><Button bsSize="xsmall" onClick={ function() {this.props.updateCurrentPath(path + key)}.bind(this) } bsStyle="link">{key}</Button></div>
-      }
+      if (value.deleted) return;
+      var folderInnards;
+      if (value.isFolder) folderInnards = this.buildInner(value, depth+1, path+key+'/');
+      return (
+        <div>
+          <div style={style}><Glyphicon glyph={value.isFolder ? 'folder-open' : 'file'}/><Button bsSize="xsmall" onClick={this.handleClick(path+key)} bsStyle="link">{key}</Button></div>
+          {folderInnards}
+        </div>
+      );
     }.bind(this));
+  },
+  getInitialState: function() {
+    return {path: this.buildDirectory(this.props.fileTree, 0, '')};
   },
 
   render: function () {
-    var path = this.buildDirectory(this.props.fileTree, 0, '');
+    //var path = this.buildDirectory(this.props.fileTree, 0, '');
 
     return (
-        <Well bsSize='small'>{ path }</Well>
+        <Well bsSize='small'>{ this.state.path }</Well>
       )
   }
 });
