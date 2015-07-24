ab36d82a283aef0457dc7201f2c05d2324c4611a
Devon <devonharvey@gmail.com>
2015-07-24 11:23:49 -0700

    (feat)Display error message on landing page if repo fetch fails

--- client/js/views/landing.react.jsx
+++ client/js/views/landing.react.jsx
@@ -6,6 +6,7 @@ var ButtonInput = ReactBootstrap.ButtonInput;
 
 var Landing = React.createClass({
   mixins : [Navigation],
+  errorMessages: {badRepo: 'Unable to fetch the requested repository. You may only gitualize public repositories.'},
 
   handleSubmit: function(e) {
     e.preventDefault();
@@ -37,10 +38,19 @@ var Landing = React.createClass({
   },
 
   render: function() {
+    if (this.props.query.error) {
+      var errorMessage = (
+          <div className='error-message'>
+            Error: {this.errorMessages[this.props.query.error]}
+          </div>
+        )
+    }
+
     return (
       <form className='repoForm' onSubmit={this.handleSubmit}>
         <Input type='text' ref='repo' label='Visualize a repo' onChange={this.handleChange} placeholder='user/reponame - try tchan247/blog-project'/>
         <ButtonInput type='submit' value='Gitualize' bsStyle={this.state.style} disabled={this.state.disabled} />
+        {errorMessage}
       </form>
     );
   }
