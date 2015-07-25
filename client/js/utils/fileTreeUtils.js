0799c71366cbe8174d93eae6b36ea1402a7b12f3
Xue Hu <dxuehu@gmail.com>
2015-07-24 19:31:42 -0700

    (Refactor) Refactor current file tree (not trie yet) and directory view

--- client/js/utils/fileTreeUtils.js
+++ client/js/utils/fileTreeUtils.js
@@ -1,11 +1,14 @@
-var updateFiles = function(currentCommit, fileTree) {
+var _ = require('underscore');
+module.exports.updateTree = function(currentCommit, fileTree) {
   var filepath;
   currentCommit.files.forEach(function(file) {
-    filepath = file.filename;
-    file.status === 'removed' ? removeFile(fileTree, filepath) : addFile(fileTree, filepath);
+    //filepath = file.filename;
+    file.status === 'removed' ? removeFile(fileTree, file) : addFile(fileTree, file);
   });
 };
-var addFile = function (tree, filePath) {
+var addFile = function (tree, file) {
+  var filePath = file.filename;
+  if (filePath === 'docs/images/lens.jpg') debugger;
   var path = filePath.split('/');
   var currentFolder = tree;
   var folderMatch, folder;
@@ -20,39 +23,40 @@ var addFile = function (tree, filePath) {
     if (!folderMatch) {
       var index = filePath.lastIndexOf('/');
       var folderPath = filePath.slice(0, index > -1? index : filePath.length);
-      currentFolder[path[0]] = {isFolder: true, path: folderPath};
+      //currentFolder[path[0]] = {isFolder: true, path: folderPath};
+      currentFolder[path[0]] = {_folderDetails: {isFolder: true, path: folderPath, value: path[path.length-2]}};
+      //isFolder if there is something in the obj other than details?
       currentFolder = currentFolder[path[0]];
     }
     path.shift();
   }
-  currentFolder[path[0]] = {isFolder: false, path: filePath};
+  currentFolder[path[0]] = {_folderDetails: {isFolder: false, path: filePath, url: file.raw_url}};
 };
 
-var removeFile = function (tree, filePath) {
+var removeFile = function (tree, file) {
+  var filePath = file.filename;
   var path = filePath.split('/');
-  var currentFolder = tree;
-  var folderMatch, folder;
+  var currentFolder = tree, parentFolder;
+  var folderOrFileName, targetFolderOrFileName;
   while (path.length > 1) {
-    folderMatch = false;
-    for (folder in currentFolder) {
-      if (folder === path[0]) {
-        currentFolder = currentFolder[folder];
-        folderMatch = true;
+    targetFolderOrFileName = path[0];
+    var found = _.find(currentFolder, function(contents, folderOrFileName) {
+      if (folderOrFileName === targetFolderOrFileName) {
+        parentFolder = currentFolder;
+        currentFolder = parentFolder[folderOrFileName];
+        return true;
       }
-    }
-    if (!folderMatch) {
-      console.log('Folder not found');
-      return;
-    }
+    });
+    if (!found) return console.log('File or Folder not found in our tree');
     path.shift();
   }
-  delete currentFolder[path[0]];
-  // if the currentFolder is empty, delete the folder
-  if (Object.keys(currentFolder).length === 2) {
-    //TODO: actually delete the folder instead of setting the property deleted
-    currentFolder.deleted = true;
+  delete currentFolder[targetFolderOrFileName]; //delete the file
+
+  // if the currentFolder is now empty, delete the folder
+  if (_.every(currentFolder, function(contents, filename) { //detect if folder empty
+    return (filename === '_folderDetails'); //if there is only folderDetails in folder
+  })) {
+    var folderName = currentFolder._folderDetails.value;
+    parentFolder && delete parentFolder[folderName]; //delete the folder if it's empty now
   }
 };
-
-module.exports = {addFile: addFile, removeFile: removeFile, updateFiles: updateFiles};
-
--- client/js/views/directory.react.jsx
+++ client/js/views/directory.react.jsx
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var ReactBootstrap = require('react-bootstrap');
 var React = require('react');
 var Button = ReactBootstrap.Button;
@@ -5,48 +6,27 @@ var Glyphicon = ReactBootstrap.Glyphicon;
 var Well = ReactBootstrap.Well;
 
 var Directory = React.createClass({
+  padStyle: function(depth) {
+    return { paddingLeft: (depth*10) + 'px' };
+  },
+  formatTree: function(tree, depth) {
+    var depth = depth || 0;
+    return _.map(tree, function(contents, filename) {
+      if (filename === '_folderDetails') return;
+      var details = contents._folderDetails;
+      return (<div>
+          <div style={this.padStyle(depth+1)}><Glyphicon glyph={details.isFolder ? 'folder-open' : 'file'}/><Button bsSize="xsmall" onClick={this.handleClick.bind(this, details.path)} bsStyle="link">{filename}</Button></div>
+          {details.isFolder ? this.formatTree(contents, depth+1) : null}
+        </div>);
+    }.bind(this));
+  },
   handleClick: function(path) {
-    debugger;
     this.props.updateCurrentPath(path);
   },
-  //map: function(obj, callback) {
-    //var array = [];
-    //for (var key in obj) {
-      //if (key !== 'isFolder' && key !== 'path' && key !== 'deleted') {
-        //array.push(callback(obj[key], key));
-      //}
-    //}
-    //return array;
-  //},
-  //buildInner: function(value, depth, path) {
-   //return (<div>{this.buildDirectory(value, depth, path)}</div>);
-  //},
-  //buildDirectory: function(obj, depth, path) {
-    //var style = {
-      //paddingLeft: (depth*10) + 'px'
-    //};
-    //return this.map(obj,function(value, key) { //TODO refactor
-      //if (value.deleted) return;
-      //var folderInnards;
-      //if (value.isFolder) folderInnards = this.buildInner(value, depth+1, path+key+'/');
-      //return (
-        //<div>
-          //<div style={style}><Glyphicon glyph={value.isFolder ? 'folder-open' : 'file'}/><Button bsSize="xsmall" onClick={this.handleClick(path+key)} bsStyle="link">{key}</Button></div>
-          //{folderInnards}
-        //</div>
-      //);
-    //}.bind(this));
-  //},
-  getInitialState: function() {
-    return {path: this.props.fileTree}
-  },
-
   render: function () {
-    //var path = this.buildDirectory(this.props.fileTree, 0, '');
-
     return (
-        <Well bsSize='small'>{ this.state.path }</Well>
-      )
+      <Well bsSize='small'>{ this.formatTree(this.props.fileTree) }</Well>
+    )
   }
 });
 
