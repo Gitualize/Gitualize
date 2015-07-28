695dc29f9275892eab98ac43559751252dfaaa21
Xue Hu <dxuehu@gmail.com>
2015-07-27 19:31:19 -0700

    (Feat) Get real first commits and add sockets skeleton

--- server/commits/commitsController.js
+++ server/commits/commitsController.js
@@ -12,29 +12,42 @@ module.exports = {
     var repoOwner = req.params.repoOwner;
     var repoName = req.params.repoName;
     var repoFullName = repoOwner + '/' + repoName;
-    utils.getTotalCommits(repoFullName);
-    utils.getCommitsFromDb(repoFullName).then(function(commits) {
-      if (commits && commits.length > 0) return res.json(commits); //commits are in db
-    }).catch(function(err) {
-      console.log('commits not in db, going to github');
-      //commits not in db, go to github
-      //TODO oauth token here
-      if (!accessToken) { //redjrect to /auth with original repo request info
-        return res.json({msg: 'auth required', authUrl: '/auth?repoFullName='+repoFullName});
-        //return res.redirect('/auth?repoFullName='+repoFullName); //don't let server redirect, client should
-        //res.end();
-        //next();
-      }
-      //100 is the max # of things we can pull at a time
-      utils.getCommitsFromGithub(repoFullName, 500)
+    utils.getTotalCommits(repoFullName)
+    .then(function(totalNumCommits) {
+      utils.getCommitsFromDb(repoFullName)
       .then(function(commits) {
-        console.log('got commits from github'); //, commits);
-        res.json(commits);
-      })
-      .catch(function(error) { //repo doesn't exist msg
-        console.error(error);
-        res.send(error);
+        if (commits && commits.length > 0) return res.json(commits); //commits are in db
+      }).catch(function(err) {
+        console.log('commits not in db, going to github');
+        //commits not in db, go to github
+        if (!accessToken) { //redjrect to /auth with original repo request info
+          return res.json({msg: 'auth required', authUrl: '/auth?repoFullName='+repoFullName});
+          //return res.redirect('/auth?repoFullName='+repoFullName); //don't let server redirect, client should
+          //res.end();
+          //next();
+        }
+        //100 is the max # of things we can pull at a time
+        var maxCommits = 500;
+        maxCommits = Math.min(500, totalNumCommits);
+        console.log('total num commits of repo is ', totalNumCommits, ', we are getting ', maxCommits, ' max');
+        utils.getCommitsFromGithub(repoFullName, totalNumCommits, maxCommits)
+        .then(function(commits) {
+          //if (commits === 'finished') {
+            //debugger;
+            //return res.end('got all commits');
+          //}
+          console.log('got some commits from github'); //, commits);
+          //res.write(commits);
+          res.json(commits);
+        })
+        .catch(function(error) { //repo doesn't exist msg
+          console.error(error);
+          res.send(error);
+        });
       });
+    }).catch(function(err) {
+      console.error(err);
+      res.send(err);
     });
   }
 };
