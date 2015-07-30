a986dc455ffa0b72ea49e2fa135735837356d44a
Devon <devonharvey@gmail.com>
2015-07-30 09:03:29 -0700

    (refactor)Remove unused comments in app.js

--- client/js/app.js
+++ client/js/app.js
@@ -1,16 +1,11 @@
-//main router file
 var _ = require('underscore');
-//var socket = require('socket.io-client');
 var $ = require('jquery');
 var React = require('react');
 var Router = require('react-router');
-// var Alert = require('react-bootstrap').Alert;
-// var bootstrap = require('bootstrap');
 var Route = Router.Route;
 var RouteHandler = Router.RouteHandler;
 var DefaultRoute = Router.DefaultRoute;
 var Views = require('./views/views.react.jsx');
-//var socket = io();
 
 var App = React.createClass({
   render () {
