1b5643b5deadfce8ef583bf3aab2da4cab5dbfb2
Terry <tchan247@gmail.com>
2015-07-31 11:16:16 -0700

    (feat) update protractor tests

--- spec/e2e/spec.js
+++ spec/e2e/spec.js
@@ -59,7 +59,6 @@ describe('Gitualize', function() {
       inputField.sendKeys('tchan247/');
 
       $('.btn-warning').getCssValue('background-color').then(function(style){
-        console.log(style);
         expect(style).toEqual('rgba(240, 173, 78, 1)');
 
         inputField.sendKeys('blog-project');
@@ -229,21 +228,20 @@ describe('Gitualize', function() {
       $('#content > div > div > div > div:nth-child(4) > div:nth-child(5) > button > span:nth-child(2)')
       .click();
 
-      browser.wait(
-        $('body > div:nth-child(15) > div > div.modal.fade.in > div > div > div.modal-body > div.form-group > div > div > div:nth-child(1) > div > div > input')
-        .isElementPresent()
-        );
+      $('body > div:nth-child(6) > div > div.modal.fade.in > div > div').waitReady();
+
+      browser.sleep(1000);
       
       // input commit range start
-      $('body > div:nth-child(15) > div > div.modal.fade.in > div > div > div.modal-body > div.form-group > div > div > div:nth-child(1) > div > div > input')
+      $('body > div:nth-child(6) > div > div.modal.fade.in > div > div > div.modal-body > div > div > div > div:nth-child(1) > div > div > input')
       .sendKeys(3);
 
       // input commit range end
-      $('body > div:nth-child(15) > div > div.modal.fade.in > div > div > div.modal-body > div.form-group > div > div > div:nth-child(2) > div > div > input')
+      $('body > div:nth-child(6) > div > div.modal.fade.in > div > div > div.modal-body > div > div > div > div:nth-child(2) > div > div > input')
       .sendKeys(4);
       
-
-      $('body > div:nth-child(15) > div > div.modal.fade.in > div > div > div.modal-body > div.form-group > div > div > div:nth-child(3) > div > input')
+      // click diffualize button in modal
+      $('body > div:nth-child(6) > div > div.modal.fade.in > div > div > div.modal-body > div > div > div > div:nth-child(3) > div > input')
       .click();
 
       expect(
@@ -263,10 +261,6 @@ describe('Gitualize', function() {
     it('should load the about page', function() {
 
       expect($('#content > div > div > p').isPresent()).toBeTruthy();
-    });
-
-    it('should display information in the about page', function() {
-      
     });
 
   });
