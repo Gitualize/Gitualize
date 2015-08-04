a5fda3a5475ed6d4c574e93014bb470b8fd2e1f7
Xue Hu <dxuehu@gmail.com>
2015-08-04 16:57:14 -0700

    Cleanup references to gitualize and inc commits

--- web_start.sh
+++ web_start.sh
@@ -3,5 +3,5 @@ echo "Stopping and removing any old web container."
 docker stop web
 docker rm web
 
-echo "Running a new container from the gitpun/node:v22 image and naming the container web."
-docker run -d -p 3000:3000 -v $(pwd):/gitpun --name web --link db gitpun/node:v22
+echo "Running a new container from the gitualize/node:v22 image and naming the container web."
+docker run -d -p 3000:3000 -v $(pwd):/gitualize --name web --link db gitualize/node:v22
