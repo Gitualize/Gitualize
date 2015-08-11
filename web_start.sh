399689b6a26305993b493cf34738a06fbe5a5694
Devon <devonharvey@gmail.com>
2015-08-11 13:05:34 -0700

    (refactor)Remove obsolete scripts

--- web_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Stopping and removing any old web container."
-docker stop web
-docker rm web
-
-echo "Running a new container from the gitualize/node:v22 image and naming the container web."
-docker run -d -p 3000:3000 -v $(pwd):/gitualize --name web --link db gitualize/node:v22
